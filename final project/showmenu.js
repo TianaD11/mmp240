baf1511df4fb912034505754ba084d5a08e00788
TianaD11 <tianadouze84@yahoo.com>
2024-12-16 21:35:34 -0500

    fix code, but links have to updated

--- final project/showmenu.js
+++ final project/showmenu.js
@@ -1,8 +1,8 @@
-function showMenu(){
+function showMenu() {
     var menus = document.getElementsByClassName("nav_link");
-    for(var i = 0; i < menus.length; i++) {
-        if(menus[i]) {
-            if (menus[i].style.display === "none" || menus[i].style.display === ""){
+    for (var i = 0; i < menus.length; i++) {
+        if (menus[i]) {
+            if (menus[i].style.display === "none" || menus[i].style.display === "") {
                 menus[i].style.display = "block";
             } else {
                 menus[i].style.display = "none";
